9b906ea5687d35cf66ff1b3417dfe1c38d1cec30
nick <32414396+qurle@users.noreply.github.com>
2024-06-03 19:27:41 +0300

    Initial commit

---
+++ src/code.ts
@@ -0,0 +1,62 @@
+// Constants
+const confirmMsgs = ["Done!", "You got it!", "Aye!", "Is that all?", "My job here is done.", "Gotcha!", "It wasn't hard.", "Got it! What's next?"]
+const renameMsgs = ["Cleaned", "Affected", "Made it with", "Fixed"]
+const idleMsgs = ["All great, already", "Nothing to do, everything's good", "Any layers to affect? Can't see it", "Nothing to do, your layers are great"]
+
+// Variables
+let notification: NotificationHandler
+let selection: ReadonlyArray<SceneNode>
+let working: boolean
+let count: number = 0
+
+figma.on("currentpagechange", cancel)
+
+// Main + Elements Check
+working = true
+selection = figma.currentPage.selection
+run()
+
+async function run() {
+  // Anything selected?
+  if (selection.length)
+    for (const node of selection)
+      await mainFunction(node)
+  else
+    await mainFunction(figma.currentPage)
+  finish()
+}
+
+// Action for selected nodes
+async function mainFunction(node: SceneNode | PageNode) {
+  count++
+}
+
+// Ending the work
+function finish() {
+  working = false
+  figma.root.setRelaunchData({ relaunch: '' })
+  if (count > 0) {
+    notify(confirmMsgs[Math.floor(Math.random() * confirmMsgs.length)] +
+      " " + renameMsgs[Math.floor(Math.random() * renameMsgs.length)] +
+      " " + ((count === 1) ? "only one layer" : (count + " layers")))
+
+  }
+  else notify(idleMsgs[Math.floor(Math.random() * idleMsgs.length)])
+  setTimeout(() => { console.log("Timeouted"), figma.closePlugin() }, 3000)
+}
+
+// Show new notification
+function notify(text: string) {
+  if (notification != null)
+    notification.cancel()
+  notification = figma.notify(text)
+}
+
+// Showing interruption notification
+function cancel() {
+  if (notification != null)
+    notification.cancel()
+  if (working) {
+    notify("Plugin work have been interrupted")
+  }
+}
